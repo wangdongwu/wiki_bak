fd516b93a236666c8f87f05cb4c44a15f9bf12b9
NaixSpirit <neverlandxy.naix@gmail.com>
2013-01-29 16:50:19 +0800

    add a methods to find all methods from include Modules

---
+++ find_methods.rb
@@ -0,0 +1,21 @@
+module TestModule
+  def module_method
+  end
+end
+
+class MyClass
+
+  instance_methods.each do |m|
+    undef_method m unless m.to_s =~ /^__|^method|object_id/
+  end
+
+  include TestModule
+end
+
+a = MyClass.new
+a.methods.each do |m|
+  next if m =~ /^__|^method|object_id/
+  file, line =  a.method(m).source_location
+  puts "Method #{m} is defined in #{file}, line #{line}"
+end
+
