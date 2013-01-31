e4e361023afd2827299765ad59d64e77c1f1a7cb
NaixSpirit <neverlandxy.naix@gmail.com>
2013-01-31 14:21:17 +0800

    add uniqueness

---
+++ uniqueness_example.rb
@@ -0,0 +1,11 @@
+# $ rails console --sandbox
+# 进入沙盒模式
+
+# 在对modle进行测试时，需要创建测试数据库
+# $ rake db:test:prepare
+
+validates :email, presence: true,
+                  format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: { case_sensitive: false }
+# 验证 Email 地址的唯一性，不区分大小写, 直接把 true 换成了 case_sensitive: false，
+# Rails 会自动指定 :uniqueness 的值为 true。
